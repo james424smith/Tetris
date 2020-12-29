c27015bccbc8ec1627ff51534ce60a4a05d9effc
james424smith@gmail.com <james424smith@gmail.com>
2020-12-30 05:39:49 +0900

    Added connection for next block

--- components/GridSquare.tsx
+++ components/GridSquare.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 // Represents a grid square with a color
 
+// const GridWrapper = styled.div`
+//   border-style: solid;
+//   width: var(--tile-size);
+//   height: var(--tile-size);
+//   border-width: var(--border-width);
+//   border-left-color: var(--border-left-color);
+//   border-top-color: var(--border-top-color);
+//   border-right-color: var(--border-right-color);
+//   border-bottom-color: var(--border-bottom-color);
+//   color: ${props => props.color};
+// `;
+
 export default function GridSquare(props) {
   const classes = `grid-square color-${props.color}`
-  return <div className={classes} />
+  return <div className={classes}></div>
 }
--- components/NextBlock.tsx
+++ components/NextBlock.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import GridSquare from './GridSquare'
 import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import { shapes } from './../redux/utils/index';
 
 const NBlock = styled.div`
   display: grid;
@@ -11,7 +13,11 @@ const NBlock = styled.div`
 // Draws the "next" block view showing the next block to drop
 export default function NextBlock(props) {
 
-    const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    const nextShape = useSelector((state) => state.game.nextShape)
+    console.log("nextShape\n" + nextShape)
+    const box = shapes[nextShape][0] // Get the first rotation
+
+    // const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
     // Map the block to the grid
     const grid = box.map((rowArray, row) => {
         return rowArray.map((square, col) => {
