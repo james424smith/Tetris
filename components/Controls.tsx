4c95e24da0a3958149d0151bd7e12cf176a8c497
james424smith@gmail.com <james424smith@gmail.com>
2020-12-30 14:41:12 +0900

    Added connection to controls

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate } from '../redux/actions'
 
-const BtnGroup = styled.div`
-  grid-area: b;
-  display: flex;
-  flex-direction: row;
-`;
+// const BtnGroup = styled.div`
+//   grid-area: b;
+//   display: flex;
+//   flex-direction: row;
+// `;
 
-const ControlBtn = styled.button`
-  --size: calc(var(--title-size) * 2.5);
-  width: var(--size);
-  height: var(--size);
-  text-align: center;
-  display: block;
-  border-width: 5px;
-  border-top-color: var(--button-color-t);
-  border-left-color: var(--button-color-l);
-  border-right-color: var(--button-color-r);
-  border-bottom-color: var(--button-color-b);
-`;
+// const ControlBtn = styled.button`
+//   --size: calc(var(--title-size) * 2.5);
+//   width: var(--size);
+//   height: var(--size);
+//   text-align: center;
+//   display: block;
+//   border-width: 5px;
+//   border-top-color: var(--button-color-t);
+//   border-left-color: var(--button-color-l);
+//   border-right-color: var(--button-color-r);
+//   border-bottom-color: var(--button-color-b);
+// `;
 
 export default function Controls(props) {
+
+  const dispatch = useDispatch()
+  const isRunning = useSelector((state) => state.isRunning)
+
   return (
     <div className='controls'>
-      <button className='control-btn' onClick={(e) => {}}>Left</button>
-      <button className='control-btn' onClick={(e) => {}}>Right</button>
-      <button className='control-btn' onClick={(e) => {}}>Rotate</button>
-      <button className='control-btn' onClick={(e) => {}}>Down</button>
+      <button className='control-btn' onClick={(e) => { dispatch(moveLeft()) }}>Left</button>
+      <button className='control-btn' onClick={(e) => { dispatch(moveRight()) }}>Right</button>
+      <button className='control-btn' onClick={(e) => { dispatch(rotate()) }}>Rotate</button>
+      <button className='control-btn' onClick={(e) => { dispatch(moveDown()) }}>Down</button>
     </div>
-    )
+  )
 }
